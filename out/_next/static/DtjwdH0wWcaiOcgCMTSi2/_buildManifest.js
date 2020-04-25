7582e318fce4491f24d98f812afa992be122f472
Sonjoy Datta <sonjoy.dat@gmail.com>
2020-04-25 13:32:45 +0600

    Modify layout

--- out/_next/static/DtjwdH0wWcaiOcgCMTSi2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002Fcb1608f2.be5cf8316726ee26acf5.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
